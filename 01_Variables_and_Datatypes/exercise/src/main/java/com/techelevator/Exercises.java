f08856b6b843ea5070da9d1c7aab075856302afe
Caroline <carolineweiss0103@gmail.com>
2021-10-06 15:50:25 -0500

    exercise 3

--- 01_Variables_and_Datatypes/exercise/src/main/java/com/techelevator/Exercises.java
+++ 01_Variables_and_Datatypes/exercise/src/main/java/com/techelevator/Exercises.java
@@ -1,4 +1,4 @@
-package com.techelevator;
+Xpackage com.techelevator;
 
 public class Exercises {
 
@@ -26,6 +26,9 @@ public class Exercises {
         3. 3 raccoons are playing in the woods. 2 go home to eat dinner. How
         many raccoons are left in the woods?
         */
+		int raccoonsInWoods = 3;
+		int raccoonsEatingDinner = 2;
+		int raccoonsRemainingInWoods = raccoonsInWoods - raccoonsEatingDinner;
 
 
         /* Exercise 4
