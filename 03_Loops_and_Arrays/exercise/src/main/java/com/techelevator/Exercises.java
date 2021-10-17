8c1b5f78deb890a31c91e1cb93c37103f2f41f0d
Caroline <carolineweiss0103@gmail.com>
2021-10-16 22:46:10 -0500

    Finished exercises LA

--- 03_Loops_and_Arrays/exercise/src/main/java/com/techelevator/Exercises.java
+++ 03_Loops_and_Arrays/exercise/src/main/java/com/techelevator/Exercises.java
@@ -10,8 +10,13 @@ public class Exercises {
 	 firstLast6([13, 6, 1, 2, 3]) → false
 	 */
 	public boolean firstLast6(int[] nums) {
-		return false;
+
+	 if ( (nums[0] == 6) ||  (nums[nums.length-1] == 6)) {
+		return true;
 	}
+      return false;
+}
+
 
 	/*
 	 2. Given an array of ints, return true if the array is length 1 or more, and the first element and
@@ -21,6 +26,11 @@ public class Exercises {
 	 sameFirstLast([1, 2, 1]) → true
 	 */
 	public boolean sameFirstLast(int[] nums) {
+		if    ((nums.length >= 1)   &&   (nums[0] == nums[nums.length-1]))  {
+			return true;
+		}
+
+
 		return false;
 	}
 
@@ -29,7 +39,8 @@ public class Exercises {
 	 makePi() → [3, 1, 4]
 	 */
 	public int[] makePi() {
-		return new int[] {};
+
+		return new int[] {3, 1, 4};
 	}
 
 	/*
@@ -40,6 +51,12 @@ public class Exercises {
 	 commonEnd([1, 2, 3], [1, 3]) → true
 	 */
 	public boolean commonEnd(int[] a, int[] b) {
+
+		if((a[0] == b[0]) || (a[a.length - 1] == b[b.length - 1])){
+			return true;
+		}
+
+
 		return false;
 	}
 
@@ -50,7 +67,12 @@ public class Exercises {
 	 sum3([7, 0, 0]) → 7
 	 */
 	public int sum3(int[] nums) {
-		return 0;
+		int sum = 0;
+		for(int i = 0; i < nums.length; i++){
+			sum += nums[i];
+		}
+
+		return sum;
 	}
 
 	/*
@@ -61,106 +83,244 @@ public class Exercises {
 	 rotateLeft3([7, 0, 0]) → [0, 0, 7]
 	 */
 	public int[] rotateLeft3(int[] nums) {
-		return new int[] {};
+
+
+		int z  = nums[0];
+		nums[0] = nums[1];
+		nums[1] = nums[2];
+		nums[2] = z;
+
+
+		return new int[] {nums[0], nums[1], nums[2]};
 	}
 
 	/*
-	 7. Given an array of ints length 3, return a new array with the elements in reverse order, so 
-	 {1, 2, 3} becomes {3, 2, 1}.
-	 reverse3([1, 2, 3]) → [3, 2, 1]
-	 reverse3([5, 11, 9]) → [9, 11, 5]
-	 reverse3([7, 0, 0]) → [0, 0, 7]
-	 */
+   7. Given an array of ints length 3, return a new array with the elements in reverse order, so
+   {1, 2, 3} becomes {3, 2, 1}.
+   reverse3([1, 2, 3]) → [3, 2, 1]
+   reverse3([5, 11, 9]) → [9, 11, 5]
+   reverse3([7, 0, 0]) → [0, 0, 7]
+   */
 	public int[] reverse3(int[] nums) {
-		return new int[] {};
+
+		int z  = nums[0];
+		nums[0] = nums[2];
+		nums[2] = nums[0];
+
+
+		return new int[] {nums [0], nums[1], z};
 	}
 
+
+
+
+
 	/*
-	 8. Given an array of ints length 3, figure out which is larger between the first and last elements 
-	 in the array, and set all the other elements to be that value. Return the changed array.
-	 maxEnd3([1, 2, 3]) → [3, 3, 3]
-	 maxEnd3([11, 5, 9]) → [11, 11, 11]
-	 maxEnd3([2, 11, 3]) → [3, 3, 3]
-	 */
+     8. Given an array of ints length 3, figure out which is larger between the first and last elements
+     in the array, and set all the other elements to be that value. Return the changed array.
+     maxEnd3([1, 2, 3]) → [3, 3, 3]
+     maxEnd3([11, 5, 9]) → [11, 11, 11]
+     maxEnd3([2, 11, 3]) → [3, 3, 3]
+     */
 	public int[] maxEnd3(int[] nums) {
-		return new int[] {};
+
+		int max ;
+		if (nums[0] > nums [2]){
+			max = nums[0];
+		} else max = nums[2];
+
+		for (int i =0; i < nums.length; i ++){
+			nums[i] = max;
+		}
+
+
+
+		return nums;
 	}
 
-	/*
-	 9. Given an array of ints, return the sum of the first 2 elements in the array. If the array length
-	  is less than 2, just sum up the elements that exist, returning 0 if the array is length 0.
-	 sum2([1, 2, 3]) → 3
-	 sum2([1, 1]) → 2
-	 sum2([1, 1, 1, 1]) → 2
-	 */
+
+
+
+   /*
+    9. Given an array of ints, return the sum of the first 2 elements in the array. If the array length
+     is less than 2, just sum up the elements that exist, returning 0 if the array is length 0.
+    sum2([1, 2, 3]) → 3
+    sum2([1, 1]) → 2
+    sum2([1, 1, 1, 1]) → 2
+    */
+
 	public int sum2(int[] nums) {
-		return 0;
+
+		if (nums.length == 0){
+			return 0;
+		} else if (nums.length < 2){
+			return nums[0];
+		} else return nums[0] + nums[1];
+
 	}
 
+
+
+
+
+
 	/*
-	 10. Given 2 int arrays, a and b, each length 3, return a new array length 2 containing their middle 
-	 elements.
-	 middleWay([1, 2, 3], [4, 5, 6]) → [2, 5]
-	 middleWay([7, 7, 7], [3, 8, 0]) → [7, 8]
-	 middleWay([5, 2, 9], [1, 4, 5]) → [2, 4]
-	 */
+     10. Given 2 int arrays, a and b, each length 3, return a new array length 2 containing their middle
+     elements.
+     middleWay([1, 2, 3], [4, 5, 6]) → [2, 5]
+     middleWay([7, 7, 7], [3, 8, 0]) → [7, 8]
+     middleWay([5, 2, 9], [1, 4, 5]) → [2, 4]
+     */
 	public int[] middleWay(int[] a, int[] b) {
-		return new int[] {};
+
+		int[] middle = new int [2];
+
+		middle[0] = a[1];
+		middle[1] = b[1];
+
+
+
+
+		return middle;
 	}
 
+
+
+
 	/*
-	 11. Return the number of even ints in the given array. Note: the % "mod" operator computes the 
-	 remainder, e.g. 5 % 2 is 1.
-	 countEvens([2, 1, 2, 3, 4]) → 3
-	 countEvens([2, 2, 0]) → 3
-	 countEvens([1, 3, 5]) → 0
-	 */
+     11. Return the number of even ints in the given array. Note: the % "mod" operator computes the
+     remainder, e.g. 5 % 2 is 1.
+     countEvens([2, 1, 2, 3, 4]) → 3
+     countEvens([2, 2, 0]) → 3
+     countEvens([1, 3, 5]) → 0
+     */
 	public int countEvens(int[] nums) {
-		return 0;
+
+		int even  = 0;
+		for(int i = 0; i < nums.length; i ++){
+			if(nums[i] % 2 == 0){
+				even += 1;
+			}
+		}
+		return even;
 	}
 
-	/*
-	 12. Return the sum of the numbers in the array, returning 0 for an empty array. Except the number 
-	 13 is very unlucky, so it does not count and numbers that come immediately after a 13 also do 
-	 not count.
-	 sum13([1, 2, 2, 1]) → 6
-	 sum13([1, 1]) → 2
-	 sum13([1, 2, 2, 1, 13]) → 6
-	 sum13([1, 2, 2, 1, 13, 3]) → 6
-	 sum13([1, 2, 2, 1, 13, 3, 4]) → 10
-	 */
+
+
+
+   /*
+    12. Return the sum of the numbers in the array, returning 0 for an empty array. Except the number
+    13 is very unlucky, so it does not count and numbers that come immediately after a 13 also do
+    not count.
+    sum13([1, 2, 2, 1]) → 6
+    sum13([1, 1]) → 2
+    sum13([1, 2, 2, 1, 13]) → 6
+    sum13([1, 2, 2, 1, 13, 3]) → 6
+    sum13([1, 2, 2, 1, 13, 3, 4]) → 10
+    */
+
+
+
+
+
 	public int sum13(int[] nums) {
-		return 0;
+
+
+		int sum = 0, i = 0;
+
+		while (i < nums.length) {
+			if (nums[i] == 13) {
+				i += 2;
+			} else {
+				sum += nums[i];
+				i++;
+			}
+		}
+
+		return sum;
+
 	}
 
+
+
 	/*
-	 13. Given an array of ints, return true if the array contains a 2 next to a 2 somewhere.
-	 has22([1, 2, 2]) → true
-	 has22([1, 2, 1, 2]) → false
-	 has22([2, 1, 2]) → false
-	 */
+     13. Given an array of ints, return true if the array contains a 2 next to a 2 somewhere.
+     has22([1, 2, 2]) → true
+     has22([1, 2, 1, 2]) → false
+     has22([2, 1, 2]) → false
+     */
 	public boolean has22(int[] nums) {
-		return false;
-	}
-	
-	/*
-	 14. Given an array of ints, return true if the array contains no 1's and no 3's.
-	 lucky13([0, 2, 4]) → true
-	 lucky13([1, 2, 3]) → false
-	 lucky13([1, 2, 4]) → false
-	 */
-	public boolean lucky13(int[] nums) {
+
+
+		for ( int i = 0; i < nums.length; i++){
+
+			if(nums[i] == 2){
+				i+=1;
+				if(nums[i] ==2){
+
+					return true;
+
+				}
+				break;
+
+			}
+
+
+		}
+
 		return false;
 	}
 
+
+
+
+
 	/*
-	 15. Given an array of ints, return true if the sum of all the 2's in the array is exactly 8.
-	 sum28([2, 3, 2, 2, 4, 2]) → true
-	 sum28([2, 3, 2, 2, 4, 2, 2]) → false
-	 sum28([1, 2, 3, 4]) → false
-	 */
+     14. Given an array of ints, return true if the array contains no 1's and no 3's.
+     lucky13([0, 2, 4]) → true
+     lucky13([1, 2, 3]) → false
+     lucky13([1, 2, 4]) → false
+     */
+	public boolean lucky13(int[] nums) {
+
+		for (int i = 0; i < nums.length; i++) {
+			if ((nums[i] == 1) || (nums[i] == 3)) {
+				return false;
+			}
+
+		}  return true;
+
+	}
+
+
+
+
+
+
+
+	/*
+     15. Given an array of ints, return true if the sum of all the 2's in the array is exactly 8.
+     sum28([2, 3, 2, 2, 4, 2]) → true
+     sum28([2, 3, 2, 2, 4, 2, 2]) → false
+     sum28([1, 2, 3, 4]) → false
+     */
 	public boolean sum28(int[] nums) {
+		int sum = 0;
+		for (int i = 0; i < nums.length; i++){
+			if(nums[i] == 2){
+				sum+= nums[i];
+			}
+
+			if(( i == nums.length - 1) && ( sum == 8)){
+
+				return true;
+
+			}
+		}
+
 		return false;
 	}
+
+
 
 }
